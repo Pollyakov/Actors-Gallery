874b6516df46b73d6a58d129b7d4fb9801efaf5d
Ksenia <ksusha.net@gmail.com>
2020-08-03 22:05:25 +0300

    adding react-bootstrap; building jsx row of ActorsModels throw map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import ActorModel from './data-model/ActorModel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
 
 
 const actor1 = new ActorModel ("Ksenia", "Pollyak", 19820119, "/selebs", "skhdbfsd")
+
 let birthdate= actor1.ageOfActor("19820119","YYYYMMDD");
 console.log(birthdate);
 const actorsData = [];
--- src/components/ActorGallery/ActorGallery.js
+++ src/components/ActorGallery/ActorGallery.js
@@ -3,12 +3,9 @@ import { Container, Table } from 'react-bootstrap';
 import ActorGallery from './components/ActorGallery/ActorGallery';
 import ActorModel from '../../data-model/ActorModel';
 
-
 class ActorGallery extends React.Component {
-
     constructor(props) {
-        super(props);
-    }
+        super(props);}
     render() {
             let contentToRender;
                 const actorCardRow = this.props.actors.map(ActorModel =>
@@ -18,32 +15,19 @@ class ActorGallery extends React.Component {
                         <td>{ActorModel.birthday}</td>
                         <td>{ActorModel.IMDBlink}</td>
                         <td>{ActorModel.img}</td>
-                        //<td>{ActorModel.AgeofActor()}</td>
+                        {/* //<td>{ActorModel.AgeofActor()}</td> */}
                     </tr>);
-    
                 contentToRender =                     
-                    <table class="table">
+                    <Table>
                         <tbody>
                             {actorCardRow}
                         </tbody>
-                    </table>
-            }
-    
-            return (
-                <div class="container">
+                    </Table>
+                    return (
+                <Container>
                     {contentToRender}
-                </div>
+                </Container>
             );
+            }    
         }
-    
-    }
-
-    return (
-        <div>
-        { contentToRender }
-        </div>
-    );
-    }
-    }
-
 export default ActorGallery;

022d439097a56c1f02e1c94abe388332b73db07a
Ksenia <ksusha.net@gmail.com>
2020-08-11 20:37:54 +0300

    axios installing

--- src/components/ActorGallery/ActorGallery.js
+++ src/components/ActorGallery/ActorGallery.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Table, Col, Row } from 'react-bootstrap';
+import { Container, Table, Col, Row, Form } from 'react-bootstrap';
 import ActorModel from '../../data-model/ActorModel';
 import ActorComp from '../ActorComp/ActorComp';
 //In ActorGallery render multiple ActorComp components 
@@ -14,23 +14,24 @@ class ActorGallery extends React.Component {
         }
         handleOnchange = (event) => {
             this.setState({
-                value: event.target.value,
+                value: event.target.value
             });
         }
     render() 
             {
             const filteredCards = this.props.actors.filter(actor=>
                 actor.fname.includes(this.state.value)).map(filteredCard =>
-                    
                         ( 
-                    <Col lg={true}><ActorComp actor={filteredCard} key={filteredCard.toString()}/></Col>
-                        ))
-
-                        
-            return (
-                <div>   
-                <input type="search" value={this.state.value} onChange={(event)=>this.handleOnchange(event)}></input>     
-                <Container>
+                    <Col lg={3} md={4} sm={6}><ActorComp actor={filteredCard} key={filteredCard.toString()}/></Col>
+                        ))        
+            return (  
+                <div>
+                <Container> 
+                <input type="search" value={this.state.value} onChange={(event)=>this.handleOnchange(event)}></input>        
+                        {/* <Form.Group>
+                        <Form.Control size="lg" type="text" placeholder="Search" ControlInput value={this.state.value.toLowerCase()} onChange={(event)=>this.handleOnchange(event)}>
+                        </Form.Control>
+                        </Form.Group> */}
                         <Row>
                             {filteredCards} 
                         </Row>
@@ -38,6 +39,5 @@ class ActorGallery extends React.Component {
                 </div>
                    ); 
             }    
-        }
-          
+}         
 export default ActorGallery;

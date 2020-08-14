aaf996c2a377257de5391779b1b5cbddd40e4e98
Pollyakov <ksusha.net@gmail.com>
2020-08-14 03:17:14 +0300

    imports for MovieComponent

--- src/components/MovieComponent/MovieComponent.js
+++ src/components/MovieComponent/MovieComponent.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import './App.css';
+import MovieModel from './data-model/MovieModel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
 class MovieComponent extends React.Component {
     constructor(props) {
     super(props);
